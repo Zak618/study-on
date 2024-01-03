50c168c5ab2a106ceebe059eecfd9c45ff745005
Rolan <89405318+Zak618@users.noreply.github.com>
2024-01-03 13:32:16 +0300

    Полностью реализован внешний интерфейс

--- src/Form/LessonType.php
+++ src/Form/LessonType.php
@@ -1,68 +1,93 @@
 <?php
 
- namespace App\Form;
+namespace App\Form;
 
- use App\Entity\Lesson;
- use App\Form\DataTransformer\CourseToStringTransformer;
- use Symfony\Component\Form\AbstractType;
- use Symfony\Component\Form\Extension\Core\Type\HiddenType;
- use Symfony\Component\Form\Extension\Core\Type\NumberType;
- use Symfony\Component\Form\Extension\Core\Type\TextareaType;
- use Symfony\Component\Form\Extension\Core\Type\TextType;
- use Symfony\Component\Form\FormBuilderInterface;
- use Symfony\Component\OptionsResolver\OptionsResolver;
- use Symfony\Component\Validator\Constraints\Length;
- use Symfony\Component\Validator\Constraints\NotBlank;
- use Symfony\Component\Validator\Constraints\Range;
+use App\Entity\Lesson;
+use App\Form\DataTransformer\CourseToStringTransformer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Range;
 
- class LessonType extends AbstractType
- {
-     private CourseToStringTransformer $transformer;
+class LessonType extends AbstractType
+{
+    private CourseToStringTransformer $transformer;
 
-     public function __construct(CourseToStringTransformer $transformer)
-     {
-         $this->transformer = $transformer;
-     }
-     public function buildForm(FormBuilderInterface $builder, array $options): void
-     {
-         $builder
-             ->add('name', TextType::class, [
-                 'label' => 'Название',
-                 'constraints' => [
-                     new NotBlank(message: 'Название не может быть пустым'),
-                     new Length(max: 255, maxMessage: 'Название урока должно быть не более 255 символов'),
-                 ],
-             ])
-             ->add('content', TextareaType::class, [
-                 'label' => 'Содержимое урока',
-                 'constraints' => [
-                     new NotBlank(message: 'Содержимое урока не может быть пустым'),
-                 ],
-             ])
-             ->add('number', NumberType::class, [
-                 'label' => 'Порядковый номер урока',
-                 'constraints' => [
-                     new NotBlank(message: 'Порядковый номер урока не может быть пустым'),
-                     new Range(
-                         notInRangeMessage: 'Значение поля должно быть в пределах от {{ min }} до {{ max }}',
-                         min: 1,
-                         max: 10000,
-                     ),
-                 ],
-             ])
-             ->add('course', HiddenType::class)
-         ;
+    public function __construct(CourseToStringTransformer $transformer)
+    {
+        $this->transformer = $transformer;
+    }
 
-         $builder
-             ->get('course')
-             ->addModelTransformer($this->transformer);
-     }
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Название',
+                'constraints' => [
+                    new NotBlank(message: 'Название не может быть пустым'),
+                    new Length(max: 255, maxMessage: 'Название урока должно быть не более 255 символов'),
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                    'style' => 'margin-left: 20px; width: auto;' 
+                ],
+                'label_attr' => [
+                    'class' => 'font-italic',
+                    'style' => 'margin-left: 20px;'
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Содержимое урока',
+                'constraints' => [
+                    new NotBlank(message: 'Содержимое урока не может быть пустым'),
+                ],
+                'attr' => [
+                    'class' => 'form-control', 
+                    'style' => 'margin-left: 20px; width: auto;' 
+                ],
+                'label_attr' => [
+                    'class' => 'font-italic',
+                    'style' => 'margin-left: 20px;'
+                ]
+            ])
+            ->add('number', NumberType::class, [
+                'label' => 'Порядковый номер урока',
+                'constraints' => [
+                    new NotBlank(message: 'Порядковый номер урока не может быть пустым'),
+                    new Range(
+                        notInRangeMessage: 'Значение поля должно быть в пределах от {{ min }} до {{ max }}',
+                        min: 1,
+                        max: 10000,
+                    ),
+                ],
+                'attr' => [
+                    'class' => 'form-control', 
+                    'style' => 'margin-left: 20px; width: auto;'
+                ],
+                'label_attr' => [
+                    'class' => 'font-italic',
+                    'style' => 'margin-left: 20px;'
+                ]
+            ])
+            ->add('course', HiddenType::class)
+        ;
 
-     public function configureOptions(OptionsResolver $resolver): void
-     {
-         $resolver->setDefaults([
-             'data_class' => Lesson::class,
-             'course' => null,
-         ]);
-     }
- }
+        $builder
+            ->get('course')
+            ->addModelTransformer($this->transformer);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Lesson::class,
+            'course' => null,
+        ]);
+    }
+}
